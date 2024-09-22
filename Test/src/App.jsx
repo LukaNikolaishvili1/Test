130455121d7d674586f4e6505e511553f5b86081
luka <lukalukito984@gmail.com>
2024-09-22 08:50:49 -0400

    first commit

---
+++ Test/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let myName = "luka"
+
+  return (
+    <div>
+      <h1>{myName}</h1>
+    </div>
+  )
+}
+
+export default App
